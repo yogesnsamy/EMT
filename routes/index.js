f00b664837a7d9dd1cc30f859d381d55bbb1bef4
Yogeswari Narayasamy <yogeswari.narayasamy@gmail.com>
2019-10-18 09:09:04 +0800

    renamed ivm$ to EMT

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 //root route
 router.get("/", function(req, res) {
-  res.render("landing", { title: "999 TO iVMS" });
+  res.render("landing", { title: "999 TO EMT" });
 });
 
 /* GET home page. */
 router.get("/index", function(req, res, next) {
   // res.send("index");
-  res.render("index", { title: "999 TO iVMS" });
+  res.render("index", { title: "999 TO EMT" });
 });
 
 module.exports = router;
